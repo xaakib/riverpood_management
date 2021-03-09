2659d435bddb2d09d1c4b6f8af415f89107e9fca
Xaakib <64243974+xaakib@users.noreply.github.com>
2021-03-09 23:30:40 +0600

    add tod and edit todo method done

---
+++ lib/notifier/todos_notifiers.dart
@@ -0,0 +1,26 @@
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:riverpood_management/models/todo.dart';
+
+class TodoList extends StateNotifier<List<Todo>> {
+  TodoList([List<Todo> initialTodos]) : super(initialTodos ?? []);
+
+  void addTodo({String description}) {
+    state = [
+      ...state,
+      Todo(description: description),
+    ];
+  }
+
+  void editTodo({String id, String description}) {
+    state = [
+      for (final todo in state)
+        if (todo.id == id)
+          Todo(
+            id: id,
+            description: description,
+          )
+        else
+          todo
+    ];
+  }
+}
